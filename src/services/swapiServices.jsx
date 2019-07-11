9c54a2968e0df9c9b2307fa7a0ef6d327f7bd592
nikita-parkhomenko <nikita.parkhom@gmail.com>
2019-07-11 23:16:00 +0300

    moved set state object to swapi service

--- src/services/swapiServices.jsx
+++ src/services/swapiServices.jsx
@@ -23,11 +23,12 @@ class SwapiService {
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`)
-    return res.results
+    return res.results.map( this._transformPlanet)
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`)
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`)
+    return this._transformPlanet(planet)
   }
 
   async getAllStarships() {
@@ -36,6 +37,21 @@ class SwapiService {
 
   getStarship(id) {
     return this.getResource(`/starships/${id}/`)
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    const id = item.url.match(idRegExp)[1]
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
   }
 }
 
