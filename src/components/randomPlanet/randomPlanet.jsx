fd286b804e8d23f315de6b94329ab7806475a07f
nikita-parkhomenko <nikita.parkhom@gmail.com>
2019-07-11 22:55:55 +0300

    Added logic to random planet component

--- src/components/randomPlanet/randomPlanet.jsx
+++ src/components/randomPlanet/randomPlanet.jsx
@@ -14,6 +14,7 @@ class RandomPlanet extends Component {
   swapiService = new swapiService()
 
   state = {
+    id: null,
     name: null,
     population: null,
     rotationPeriod: null,
@@ -21,8 +22,10 @@ class RandomPlanet extends Component {
   }
 
   updatePlanet() {
-    this.swapiService.getPlanet(7).then( (planet) => {
+    const id = Math.floor(Math.random() * 10 )
+    this.swapiService.getPlanet(id).then( (planet) => {
       this.setState({
+        id,
         name: planet.name,
         population: planet.population,
         rotationPeriod: planet.rotation_period,
@@ -33,12 +36,12 @@ class RandomPlanet extends Component {
 
   render() {
 
-    const {name, population, rotationPeriod, diameter} = {...this.state}
+    const {id, name, population, rotationPeriod, diameter} = {...this.state}
 
     return (
     <div className="random-planet jumbotron rounded">
         <img className="planet-image" alt="planet"
-            //  src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} 
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} 
             />
         <div>
           <h4>{name}</h4>
