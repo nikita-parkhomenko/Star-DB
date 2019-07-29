84de679649ee9fd3c0929c3180164ba8900e3c0e
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2019-07-29 20:32:16 +0300

    Updates

---
+++ precache-manifest.b1cf582acbb35bee99499e572e6e33b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b1a061455ba18246156ea68dc247d8",
+    "url": "/Star-DB/index.html"
+  },
+  {
+    "revision": "65e7e7b4be17da05ad85",
+    "url": "/Star-DB/static/css/main.b67f63d2.chunk.css"
+  },
+  {
+    "revision": "d2bd215824cbaf8f574d",
+    "url": "/Star-DB/static/js/2.412d7751.chunk.js"
+  },
+  {
+    "revision": "65e7e7b4be17da05ad85",
+    "url": "/Star-DB/static/js/main.4b1b5b58.chunk.js"
+  },
+  {
+    "revision": "4e6d9ad442076a9bb6d1",
+    "url": "/Star-DB/static/js/runtime~main.fd36be8f.js"
+  },
+  {
+    "revision": "67547828d082ed8f9f02b0c73c3139f0",
+    "url": "/Star-DB/static/media/error-star.67547828.png"
+  }
+]);
